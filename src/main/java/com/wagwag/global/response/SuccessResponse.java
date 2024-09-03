d4167504bbaa2902d194750b973a87987ac10a16
MyeongIn Jin <113578420+myeonginjin@users.noreply.github.com>
2024-09-03 20:57:00 +0900

    Update SuccessResponse.java

--- src/main/java/com/wagwag/global/response/SuccessResponse.java
+++ src/main/java/com/wagwag/global/response/SuccessResponse.java
@@ -5,20 +5,20 @@ import lombok.Getter;
 
 
 @Getter
-public class SuccessResponse<T> extends ApiResponse{
+public class SuccessResponse<T> extends ApiResponse {
 
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private final T result;
 
-    private SuccessResponse(T data){
+    private SuccessResponse(T data) {
         super(true, "200", "OK");
         this.result = data;
     }
 
-    public static <T> SuccessResponse<T> success(T data){
+    public static <T> SuccessResponse<T> success(T data) {
         return new SuccessResponse<>(data);
     }
-    public static <T> SuccessResponse<T> successWithoutResult(T data){
+    public static <T> SuccessResponse<T> successWithoutResult(T data) {
         return new SuccessResponse<>(null);
     }
 }
